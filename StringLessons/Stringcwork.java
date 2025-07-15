80ec7069fb08edd90bec80d70489ff49907e3d99
MIRALI2012AzeK <sahibemansurova@gmail.com>
2025-07-15 12:40:27 +0400

    urok string

---
+++ StringLessons/Stringcwork.java
@@ -0,0 +1,13 @@
+package StringLessons;
+
+public class Stringcwork {
+    public static void main(String[] args) {
+        String str1 = "Mirali";
+        String str2 = "Mansurov";
+
+        System.out.println(str1.length());
+        System.out.println(str1.charAt(0));
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1.equals(str2));
+    }
+}
